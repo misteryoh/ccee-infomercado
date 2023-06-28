e052ed57a3fd33ff0776ca7a3ccc28a18b94d748
Misteryoh <oliver.akio@gmail.com>
2023-06-28 14:13:26 -0300

    lambda function

--- ccee_datastructor.py
+++ ccee_datastructor.py
@@ -1,41 +1,88 @@
 import openpyxl
 import pandas
 
-workbook = openpyxl.load_workbook('/home/misteryoh/Coding/Git/ccee-infomercado/data/InfoMercado.xlsx', read_only=True)
+def lambda_handler(event, context):
 
-sheet = workbook['002 Usinas']
+    """Extract structured data from downloaded file from CCEE
+    Payload exemple
+    {
+        "filepath"    : "/home/misteryoh/Coding/Git/ccee-infomercado/data",
+        "filename"    : "InfoMercado.xlsx",
+        "profile"     : "default",
+        "params" : {
+            "sheet_name" : "002 Usinas",
+            "table_name" : "Tabela 001",
+            "first_col"  : "Código do Ativo",
+            "last_col"   : "Geração por Unit Commitment",
+            "footer"     : "Topo"
+        }
+    }
+    """
 
-first_col_identifier = "Código do Ativo"
-last_col_identifier = "Geração por Unit Commitment"
-foot_row_identifier = "Topo"
+    filepath = event['filepath']
+    filename = event['filename']
+    profile = event['profile']
+    params = event['params']
 
-start_cell = None
-last_cell = None
-foot_cell = None
+    workbook = openpyxl.load_workbook(filepath + "/" + filename, read_only=True)
 
-for row in sheet.iter_rows():
-    for cell in row:
-        if first_col_identifier in str(cell.value):
-            start_cell = cell
-        if last_col_identifier in str(cell.value):
-            last_cell = cell
-        if foot_row_identifier in str(cell.value):
-            foot_cell = cell
-            break
+    sheet = workbook[params['sheet_name']]
 
-print("start_cell  : " + start_cell.coordinate)
-print("last_cell   : " + last_cell.column_letter + str(foot_cell.row - 3))
+    title_cell = params['table_name']
+    first_cell = params['first_col']
+    last_cell = params['last_col']
+    footer_cell = params['footer']
 
-data_rows = []
-for row in sheet[f'{start_cell.coordinate}':f'{last_cell.column_letter + str(foot_cell.row - 3)}']:
-    data_cols = []
-    for cell in row:
-        data_cols.append(cell.value)
-    data_rows.append(data_cols)
+    title_range = None
+    start_range = None
+    last_range = None
+    foot_range = None
 
-df = pandas.DataFrame(data_rows)
+    for row in sheet.iter_rows():
+        for cell in row:
+            if title_cell in str(cell.value):
+                title_range = cell
+            if first_cell in str(cell.value):
+                start_range = cell
+            if last_cell in str(cell.value):
+                last_range = cell
+            if footer_cell in str(cell.value):
+                foot_range = cell
+                break
 
-df.columns = df.iloc[0]
-df = df[1:]
+    print("title_range  : " + title_range.coordinate)
+    print("start_range  : " + start_range.coordinate)
+    print("last_range  : " + last_range.coordinate)
+    print("foot_range   : " + foot_range.coordinate)
+
+    data_rows = []
+    if title_range.value is not None:
+        for row in sheet[f'{start_range.coordinate}':f'{last_range.column_letter + str(foot_range.row - 3)}']:
+            data_cols = []
+            for cell in row:
+                data_cols.append(cell.value)
+            data_rows.append(data_cols)
+    else:
+        print("Algo deu errado")
+
+    df = pandas.DataFrame(data_rows)
+
+    return df
+
+
+payload = {
+    "filepath"    : "/home/misteryoh/Coding/Git/ccee-infomercado/data",
+    "filename"    : "InfoMercado.xlsx",
+    "profile"     : "default",
+    "params" : {
+        "sheet_name" : "002 Usinas",
+        "table_name" : "Tabela 001",
+        "first_col"  : "Código do Ativo",
+        "last_col"   : "Geração por Unit Commitment",
+        "footer"     : "Topo"
+    }
+}
+
+df = lambda_handler(event=payload, context=None)
 
 print(df.head())
