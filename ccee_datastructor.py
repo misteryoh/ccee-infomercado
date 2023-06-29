7cacd42f5114ab2a2956c2edf488dc05835439f2
Misteryoh <oliver.akio@gmail.com>
2023-06-29 00:48:36 -0300

    add loop to extract multiple sheets data

--- ccee_datastructor.py
+++ ccee_datastructor.py
@@ -9,13 +9,24 @@ def lambda_handler(event, context):
         "filepath"    : "/home/misteryoh/Coding/Git/ccee-infomercado/data",
         "filename"    : "InfoMercado.xlsx",
         "profile"     : "default",
-        "params" : {
-            "sheet_name" : "002 Usinas",
-            "table_name" : "Tabela 001",
-            "first_col"  : "Código do Ativo",
-            "last_col"   : "Geração por Unit Commitment",
-            "footer"     : "Topo"
-        }
+        "sheets" : [
+            {
+                "sheet_name" : "002 Usinas",
+                "table_name" : "Tabela 001",
+                "first_col"  : "Código do Ativo",
+                "last_col"   : "Geração por Unit Commitment",
+                "footer"     : "Topo",
+                "deadrows"   : 3
+            },
+            {
+                "sheet_name" : "007 Lista de Perfis",
+                "table_name" : "Tabela 001",
+                "first_col"  : "Cód. Agente",
+                "last_col"   : "Perfil Varejista",
+                "footer"     : "Topo",
+                "deadrows"   : 2
+            }
+        ]
     }
     """
 
@@ -23,66 +34,85 @@ def lambda_handler(event, context):
     filename = event['filename']
     profile = event['profile']
     params = event['params']
+    results = []
 
     workbook = openpyxl.load_workbook(filepath + "/" + filename, read_only=True)
 
-    sheet = workbook[params['sheet_name']]
+    for sheets in params:
 
-    title_cell = params['table_name']
-    first_cell = params['first_col']
-    last_cell = params['last_col']
-    footer_cell = params['footer']
+        sheet = workbook[sheets['sheet_name']]
 
-    title_range = None
-    start_range = None
-    last_range = None
-    foot_range = None
+        title_cell = sheets['table_name']
+        first_cell = sheets['first_col']
+        last_cell = sheets['last_col']
+        footer_cell = sheets['footer']
+        deadrows = sheets['deadrows']
 
-    for row in sheet.iter_rows():
-        for cell in row:
-            if title_cell in str(cell.value):
-                title_range = cell
-            if first_cell in str(cell.value):
-                start_range = cell
-            if last_cell in str(cell.value):
-                last_range = cell
-            if footer_cell in str(cell.value):
-                foot_range = cell
-                break
+        title_range = None
+        start_range = None
+        last_range = None
+        foot_range = None
 
-    print("title_range  : " + title_range.coordinate)
-    print("start_range  : " + start_range.coordinate)
-    print("last_range  : " + last_range.coordinate)
-    print("foot_range   : " + foot_range.coordinate)
-
-    data_rows = []
-    if title_range.value is not None:
-        for row in sheet[f'{start_range.coordinate}':f'{last_range.column_letter + str(foot_range.row - 3)}']:
-            data_cols = []
+        for row in sheet.iter_rows():
             for cell in row:
-                data_cols.append(cell.value)
-            data_rows.append(data_cols)
-    else:
-        print("Algo deu errado")
+                if title_cell in str(cell.value):
+                    title_range = cell
+                if first_cell in str(cell.value):
+                    start_range = cell
+                if last_cell in str(cell.value):
+                    last_range = cell
+                if footer_cell in str(cell.value):
+                    foot_range = cell
+                    break
 
-    df = pandas.DataFrame(data_rows)
+        print("title_range  : " + title_range.coordinate)
+        print("start_range  : " + start_range.coordinate)
+        print("last_range  : " + last_range.coordinate)
+        print("foot_range   : " + foot_range.coordinate)
 
-    return df
+        data_rows = []
+        if title_range.value is not None:
+            for row in sheet[f'{start_range.coordinate}':f'{last_range.column_letter + str(foot_range.row - deadrows)}']:
+                data_cols = []
+                for cell in row:
+                    data_cols.append(cell.value)
+                data_rows.append(data_cols)
+        else:
+            print("Algo deu errado")
 
+        df = pandas.DataFrame(data_rows)
+        df.columns = df.iloc[0]
+        df = df[1:]
+
+        results.append(df)
+
+    return results
 
 payload = {
     "filepath"    : "/home/misteryoh/Coding/Git/ccee-infomercado/data",
     "filename"    : "InfoMercado.xlsx",
     "profile"     : "default",
-    "params" : {
-        "sheet_name" : "002 Usinas",
-        "table_name" : "Tabela 001",
-        "first_col"  : "Código do Ativo",
-        "last_col"   : "Geração por Unit Commitment",
-        "footer"     : "Topo"
-    }
+    "params" : [
+        {
+            "sheet_name" : "002 Usinas",
+            "table_name" : "Tabela 001",
+            "first_col"  : "Código do Ativo",
+            "last_col"   : "Geração por Unit Commitment",
+            "footer"     : "Topo",
+            "deadrows"   : 3
+        },
+        {
+            "sheet_name" : "007 Lista de Perfis",
+            "table_name" : "Tabela 001",
+            "first_col"  : "Cód. Agente",
+            "last_col"   : "Perfil Varejista",
+            "footer"     : "Topo",
+            "deadrows"   : 2
+        }
+    ]
 }
 
-df = lambda_handler(event=payload, context=None)
+results = lambda_handler(event=payload, context=None)
 
-print(df.head())
+for df in results:
+    print(df.head())
